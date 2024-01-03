939f7eaad8ea762d8c5f2f1664d7da6194555276
Carlos Perales <carlosperales050@gmail.com>
2024-01-03 12:59:45 -0600

    style: styles to navigation tab

--- src/navigation/NavigationTab.js
+++ src/navigation/NavigationTab.js
@@ -1,15 +1,44 @@
 import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/FontAwesome5";
 import HomeScreen from "../screen/HomeScreen";
 import ScoreScreen from "../screen/ScoreScreen";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Tab = createBottomTabNavigator();
 
 export default function NavigationTab() {
   return (
-    <Tab.Navigator initialRouteName="Home" screenOptions={{}}>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Score" component={ScoreScreen} />
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: "#1d1d1d",
+        },
+        tabBarActiveTintColor: "#bb86fc",
+        tabBarInactiveBackgroundColor: "#121212",
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        options={{
+          tabBarLabel: "Game",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="gamepad" color={color} size={size} />
+          ),
+        }}
+        component={HomeScreen}
+      />
+      <Tab.Screen
+        name="Score"
+        component={ScoreScreen}
+        options={{
+          tabBarLabel: "Scores",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="star" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
