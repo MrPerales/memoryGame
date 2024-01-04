fd5674e67c01f02698e53e2505e3c727f0130d4d
Carlos Perales <carlosperales050@gmail.com>
2024-01-03 18:26:42 -0600

    add: ScoreScreen and ScoreTable

---
+++ src/components/ScoreTable.js
@@ -0,0 +1,58 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+export default function ScoreTable({ position }) {
+  const positionName = (position) => {
+    if (position === 1) {
+      return "ST";
+    } else if (position === 2) {
+      return "ND";
+    } else if (position === 3) {
+      return "RD";
+    } else if (position >= 4) {
+      return "TH";
+    }
+  };
+  const positionStyle = (position) => {
+    let txtColor;
+    if (position <= 3) {
+      txtColor = "#6200ee";
+    } else if (position > 3 && position <= 6) {
+      txtColor = "#7f39fb";
+    } else if (position > 6 && position <= 9) {
+      txtColor = "#985eff";
+    } else if (position === 10) {
+      txtColor = "#dbb2ff";
+    }
+    return {
+      color: txtColor,
+    };
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.text, positionStyle(position)]}>
+        {`${position} ${positionName(position)}`}
+      </Text>
+      <Text style={[styles.text, positionStyle(position)]}>99999 PTS</Text>
+      <Text style={[styles.text, positionStyle(position)]}>AAA</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    borderRadius: 20,
+    borderWidth: 3,
+    borderColor: "#2d2d2d",
+    marginBottom: 10,
+  },
+  text: {
+    width: 100,
+    height: 40,
+    padding: 10,
+    textAlign: "center",
+    fontFamily: "pokemonSolidNormal",
+    fontSize: 12,
+  },
+});
--- src/screen/ScoreScreen.js
+++ src/screen/ScoreScreen.js
@@ -1,14 +1,34 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import React from "react";
+import ScoreTable from "../components/ScoreTable";
 
 export default function ScoreScreen() {
   return (
-    <View>
-      <Text>ScoreScreen</Text>
-      <Text>ScoreScreen</Text>
-      <Text>ScoreScreen</Text>
-      <Text>ScoreScreen</Text>
-      <Text>ScoreScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Best Players</Text>
+      <ScrollView style={styles.tableScoreContainer}>
+        {bestPlayers.map((_, index) => (
+          <ScoreTable key={index} position={index + 1} />
+        ))}
+      </ScrollView>
     </View>
   );
 }
+const bestPlayers = [1, 2, 3, 3, 2, 2, 5, 85, 1, 2];
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121212",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "#fff",
+    fontFamily: "pokemonSolidNormal",
+    fontSize: 30,
+    marginTop: 60,
+  },
+  tableScoreContainer: {
+    marginTop: 15,
+  },
+});
